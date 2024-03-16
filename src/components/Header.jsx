8095aae4ebb9308c635435f21f895b13eae89a61
shukranjsp@gmail.com <shukranjsp@gmail.com>
2024-03-17 00:59:45 +0100

    feature: Meals and Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logoImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} />
+        <h1>React Food</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart (0)</Button>
+      </nav>
+    </header>
+  );
+}
